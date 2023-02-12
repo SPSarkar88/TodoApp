54e757ba608993c702b354b50ff23b237cc2ffd8
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2023-02-12 23:14:30 +0530

    Reformat

--- SQL/GetById.sql
+++ SQL/GetById.sql
@@ -1,44 +1,21 @@
-IF OBJECT_ID('dbo.usp_getItemById') IS NOT NULL
-	SET NOEXEC ON
+IF EXISTS (SELECT * FROM DBO.SYSOBJECTS WHERE ID = OBJECT_ID(N'dbo.usp_getItemById') AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
+  DROP PROCEDURE dbo.usp_getItemById
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
 GO
 CREATE PROCEDURE dbo.usp_getItemById
-AS RETURN;
-GO
-SET NOEXEC OFF
-GO
-ALTER PROCEDURE dbo.usp_getItemById
 (
 	@Id UNIQUEIDENTIFIER
 )
 -- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
 AS BEGIN
 	SET NOCOUNT ON;
-	BEGIN TRY
-		BEGIN TRANSACTION 
-			SELECT tt.ID, tt.Item, tt.ItemDescription, tt.CreatedDate, tt.UpdatedDate, tt.CompletedDate, tt.Status FROM TblTodo tt WHERE tt.ID = @Id AND tt.IsDeleted <> 0
-		COMMIT TRANSACTION
-    END TRY
-	BEGIN CATCH
-		BEGIN TRANSACTION
-			INSERT INTO dbo.DB_Errors
-			VALUES
-		  (
-			SUSER_SNAME(),
-			ERROR_NUMBER(),
-			ERROR_STATE(),
-			ERROR_SEVERITY(),
-			ERROR_LINE(),
-			ERROR_PROCEDURE(),
-			ERROR_MESSAGE(),
-			GETDATE());
- 
-		-- Transaction uncommittable
-			IF (XACT_STATE()) = -1
-				ROLLBACK TRANSACTION
- 
-		-- Transaction committable
-			IF (XACT_STATE()) = 1
-				COMMIT TRANSACTION
-	END CATCH
+	SELECT tt.ID, tt.Item, tt.ItemDescription, tt.CreatedDate, tt.UpdatedDate, tt.CompletedDate, tt.Status 
+	FROM TblTodo tt 
+	WHERE tt.ID = @Id AND tt.IsDeleted <> 0
 END
 GO
--- SQL/Table.sql
+++ SQL/Table.sql
@@ -12,17 +12,3 @@ CREATE TABLE dbo.TblTodo
 	CONSTRAINT PK_TodoTbl_ID PRIMARY KEY (ID)
 )
 GO
-
-CREATE TABLE DB_Errors
-(
-	ErrorID        INT IDENTITY(1, 1),
-	UserName       VARCHAR(100),
-	ErrorNumber    INT,
-	ErrorState     INT,
-	ErrorSeverity  INT,
-	ErrorLine      INT,
-	ErrorProcedure VARCHAR(MAX),
-	ErrorMessage   VARCHAR(MAX),
-	ErrorDateTime  DATETIME
-)
-GO
